a643a008200c549fef7eb4abc1f9d232adefcd53
AREEBA-FATIMA <areebafatima2457@gmail.com>
2024-10-03 18:44:53 +0500

    credify project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import "./globals.css";
+import { ReactNode } from "react";
 
 export default function RootLayout({
   children,
-}: any) {
+}: { children: ReactNode }) {
   return (
     <html>
       <head>
@@ -19,23 +20,22 @@ export default function RootLayout({
             </a>
 
             <nav className="navbar">
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#options">Options</a>
-                <a href="#contact">Contact</a>
-                <a href="#blogs">Blogs</a>
+              <a href="#home">Home</a>
+              <a href="#about">About</a>
+              <a href="#options">Options</a>
+              <a href="#contact">Contact</a>
+              <a href="#blogs">Blogs</a>
             </nav>
 
-            <a href="#" className="btn">Get Started</a>
-            
-            <div id="menu-btn" className="fas fa-bars-staggered"></div>
+            <a href="#" className="btn">
+              Get Started
+            </a>
 
+            <div id="menu-btn" className="fas fa-bars-staggered"></div>
           </section>
         </header>
         {children}
-        <head>
-          <script src="/js/script.js"></script>
-        </head>
+        <script src="/js/script.js" async></script>
       </body>
     </html>
   );
